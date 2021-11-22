b7b6470f067437c2804bae326a4f041bcb27da81
nawrocki97@gmail.com <nawrocki97@gmail.com>
2021-11-22 14:20:49 +0100

    Add main menu and game manager

---
+++ Assets/Scripts/SaveData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveData
+{
+    public string CurrentLevel;
+    // public sthsth..checkpoint Checkpoint;
+
+    // public int Points
+    // etc....
+}
