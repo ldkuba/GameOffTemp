c0aa8bc451d083bb61b0a829344dd52e7a1c027c
Archit <architk1105@gmail.com>
2021-12-01 17:45:32 +0530

    added sound to vicotry scene and added credits to it. added sound to buttons in victory scene and the main menu. changed soundtrack for levels.

--- Assets/Scripts/UI/VictoryScene.cs
+++ Assets/Scripts/UI/VictoryScene.cs
@@ -7,9 +7,29 @@ public class VictoryScene : MonoBehaviour
 {
     [SerializeField]
     private Button m_goToMainMenu;
-    
+    [SerializeField]
+    private Button m_devTeamTrigger;
+    [SerializeField]
+    private AudioClip m_buttonPressClip;
+    private CanvasRenderer m_devNames;
+
+    public void Start()
+    {
+        m_devNames = GameObject.Find("DevNames").GetComponent<CanvasRenderer>();
+        m_devNames.SetAlpha(0f);    
+    }
+
     public void OnClickMainMenu()
     {
+        AudioSource.PlayClipAtPoint(m_buttonPressClip, transform.position);
         GameManager.Instance.ExitToMenu();
+    }
+
+    public void onClickDevTeamTrigger()
+    {
+        
+        AudioSource.PlayClipAtPoint(m_buttonPressClip, transform.position);
+        m_devNames.SetAlpha(1f);
+        
     }
 }
